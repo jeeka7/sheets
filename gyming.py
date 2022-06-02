13a85acf02a5da5552eb55fb6dd2010ee55836b8
Dean Hunter <jeeka7@gmail.com>
2022-06-03 03:51:56 +0530

    Update gyming.py

--- gyming.py
+++ gyming.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write("I wish i could learn to interact with database and private sheets")
+st.write(" learning to interact with database and private sheets")
 
 # streamlit_app.py
 
